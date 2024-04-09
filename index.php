d6492f55a3a43c4f1a2c88ff588a4f55830c7616
Mason Sain <sainmason012@gmail.com>
2024-04-09 13:42:37 -0400

    Add view homepage

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-   echo '<h1>Hello Fat-Free!</h1>';
+   //echo '<h1>Hello Fat-Free!</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run Fat-Free
